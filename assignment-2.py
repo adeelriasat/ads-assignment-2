e3d563a6c92d97cb320bddcaebe6965c913c7ad4
adeelriasat <adeelriasat60@gmail.com>
2023-04-04 03:48:57 +0100

    cleaning the dfs done

--- assignment-2.py
+++ assignment-2.py
@@ -29,6 +29,10 @@ def getDataFramesFromCsv(filePath, sRows=0, nonNumericalRows=0):
     # remove non numerical data from df
     df1 = df1.iloc[:, nonNumericalRows:]
     
+    # clean the dataframe first remove all columns which are empty 
+    # then all rows which are empty and then those with missing entries
+    df1 = df1.dropna(axis=1, how='all').dropna(how='all').dropna()
+
     # get second dataframe countries as column
     # for this purpose transpose the dataframe
     df_transposed = csv_data.transpose()
@@ -40,12 +44,20 @@ def getDataFramesFromCsv(filePath, sRows=0, nonNumericalRows=0):
     df_transposed = df_transposed.iloc[nonNumericalRows:]
     
     # rename the index column
-    df_transposed.index.name = 'year'
+    df_transposed.index.name = 'Year'
+    
+    # clean the transposed dataframe those are completely empty and then drop column with missing entries
+    df_transposed = df_transposed.dropna(how='all').dropna(axis=1)    
+    
+    
+    df_transposed.index = pd.to_numeric(df_transposed.index)
+
     
     return df1, df_transposed
 
 
 df_yearsAsColumn, df_countryAsColumn = getDataFramesFromCsv(
-    '../API_EG.USE.ELEC.KH.PC_DS2_en_csv_v2_5359189/API_EG.USE.ELEC.KH.PC_DS2_en_csv_v2_5359189.csv', sRows=4, nonNumericalRows=4)
+    '../API_EG.ELC.PETR.ZS_DS2_en_csv_v2_5362831/API_EG.ELC.PETR.ZS_DS2_en_csv_v2_5362831.csv', sRows=4, nonNumericalRows=4)
 df_countryAsColumn.to_csv('../test1.csv')
+df_yearsAsColumn.to_csv('../test.csv')
 print(df_countryAsColumn)
