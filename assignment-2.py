186969f7bb0af8bb23efa61478a22191fb18367c
adeelriasat <adeelriasat60@gmail.com>
2023-04-04 02:30:29 +0100

    method to get dataframes
    First point this method will take csv filepath as an argument and will return
    2 dataframes one with years as column
    and 1 with countries as column

--- assignment-2.py
+++ assignment-2.py
@@ -6,3 +6,19 @@ Created on Tue Mar 28 01:00:11 2023
 """
 
 import numpy as np
+import pandas as pd
+
+
+def getDataFramesFromCsv(filePath, sRows=0):
+    """ Method to return dataframes:
+        This method will take filepath and sRows
+        as params and will return 2 dataframes 
+        one dataframe will as it is and the second 
+        dataframe will be transpose of the first one
+        filePath: Csv path
+        sRows: number of rows to skip from start default is 0
+    """
+    # get first datafrom years as column
+    df1 = pd.read_csv(filePath, skiprows=sRows)
+    return df1,df2
+    
